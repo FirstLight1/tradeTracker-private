c0e616ebed9feb3a3d52ad7e593d3eee068513b4
Lukas Kamen <kamen.lukas1990@gmail.com>
2026-06-03 13:23:49 +0200

    Fix

--- tradeTracker/actions.py
+++ tradeTracker/actions.py
@@ -362,8 +362,7 @@ def loadAuctions():
     # Auto-migrate payment_method data on load
     auctions_list = []
     for auction in auctions:
-        auction_dict = dict(auction)
-        """
+        auction_dict = dict(auction) 
         if auction_dict.get('payment_method'):
             # Check if migration needed
             migrated = migrate_payment_method(auction_dict['payment_method'])
@@ -373,7 +372,7 @@ def loadAuctions():
                           (migrated, auction_dict['id']))
                 auction_dict['payment_method'] = migrated
         auctions_list.append(auction_dict)
-        """
+       
     db.commit()
     return jsonify(auctions_list)
 
