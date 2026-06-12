331574c2eb37224f23061577fd44b0587d4e9143
Lukas Kamen <kamen.lukas1990@gmail.com>
2026-06-12 10:34:24 +0200

    Feat: DB migrations

--- tradeTracker/db.py
+++ tradeTracker/db.py
@@ -161,6 +161,7 @@ CREATE TABLE cards (
     condition TEXT,
     card_price REAL,
     market_value REAL,
+    cardMarketID TEXT,
     FOREIGN KEY (auction_id) REFERENCES auctions (id) ON DELETE CASCADE
 );
 
@@ -240,6 +241,7 @@ CREATE TABLE sealed(
     date TEXT,
     sale_id INTEGER,
     auction_id INTEGER,
+    cardMarketID TEXT,
     FOREIGN KEY (sale_id) REFERENCES sales (id) ON DELETE CASCADE,
     FOREIGN KEY (auction_id) REFERENCES auctions(id) ON DELETE CASCADE
     );
